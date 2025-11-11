b84d27a5a635f1d4b89e8f5b9b5fefecb625fe0e
Daniel Aziegbe <danielaziegbe98@gmail.com>
2025-11-10 22:22:53 -0500

    Displayed required weather features

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const para = document.querySelector(".temperature");
+const highs = document.querySelector(".highs");
 const mainForecast = document.querySelector(".main-forecast");
 const locationQuery = document.querySelector("#location");
 const searchQuery = document.querySelector(".search-location");
@@ -9,7 +10,6 @@ async function getWeather(location) {
          `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=LZGLPSJQ6WJM8CD9NYLBG39UN`
       );
       let weatherJson = await weather.json();
-      // para.textContent = weatherJson.days[1].hours[1].temp;
       console.log(weatherJson);
       return weatherJson;
    } catch (err) {
@@ -17,26 +17,27 @@ async function getWeather(location) {
          "Error: Location not found. Please ensure you include the city/country name in your input";
       para.textContent = err;
    }
-   //   img.src = response.data.images.original.url;
 }
 
 async function specificData(location) {
    let data = await getWeather(location);
-   para.textContent = data.days[1].hours[0].temp;
+   let temp = data.currentConditions.temp - 32 * 5 / 9;
+   para.textContent = `${temp.toFixed(1)}°C || ${data.currentConditions.temp}°F`;
    mainForecast.textContent = data.resolvedAddress;
+   //    highs.textContent =
    // return data.days;
 }
 
 searchQuery.addEventListener("click", () => {
    !locationQuery
       ? specificData("st catharines")
-      : specificData(locationQuery.value)
+      : specificData(locationQuery.value);
 });
 
-locationQuery.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-     !locationQuery
-      ? specificData("st catharines")
-      : specificData(locationQuery.value)
-  }
+locationQuery.addEventListener("keydown", event => {
+   if (event.key === "Enter") {
+      !locationQuery
+         ? specificData("st catharines")
+         : specificData(locationQuery.value);
+   }
 });
